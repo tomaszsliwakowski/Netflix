60abf4d50d92c4484fdb0fd401f26e5dc529a18a
tomaszsliwakowski <tomek08082002@gmail.com>
2023-03-18 11:53:26 +0100

    Changes to be committed: 	modified:   src/App.js 	modified:   src/components/header/index.js 	modified:   src/components/header/styles/header.js 	modified:   src/components/index.js 	new file:   src/components/loading/index.js 	new file:   src/components/loading/styles/loading.js 	new file:   src/components/profiles/index.js 	new file:   src/components/profiles/styles/profiles.js 	new file:   src/containers/browse.js 	new file:   src/containers/profiles.js 	modified:   src/hooks/index.js 	new file:   src/hooks/use-content.js 	modified:   src/pages/browse.js 	new file:   src/utils/selection-filter.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,44 @@
 import React from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
-import { Background, Container, Logo, ButtonLink } from "./styles/header";
+import {
+  Background,
+  Container,
+  Logo,
+  ButtonLink,
+  Feature,
+  Text,
+  FeatureCallOut,
+  Link,
+  Group,
+} from "./styles/header";
 
 export default function Header({ bg = true, children, ...restprops }) {
   return bg ? <Background {...restprops}>{children}</Background> : children;
 }
 
+Header.Feature = function HeaderFeature({ children, ...restprops }) {
+  return <Feature {...restprops}>{children}</Feature>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restprops
+}) {
+  return <FeatureCallOut {...restprops}>{children}</FeatureCallOut>;
+};
+Header.Text = function HeaderText({ children, ...restprops }) {
+  return <Text {...restprops}>{children}</Text>;
+};
+Header.TextLink = function HeaderTextLink({ children, ...restprops }) {
+  return <Link {...restprops}>{children}</Link>;
+};
+
 Header.Frame = function HeaderFrame({ children, ...restprops }) {
   return <Container {...restprops}>{children}</Container>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restprops }) {
+  return <Group {...restprops}>{children}</Group>;
 };
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restprops }) {
---
+++ src/components/profiles/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Title, List, Picture, Name, Item } from "./styles/profiles";
+
+export default function Profiles({ children, ...restprops }) {
+  return <Container {...restprops}>{children}</Container>;
+}
+
+Profiles.Title = function ProfilesTitle({ children, ...restprops }) {
+  return <Title {...restprops}>{children}</Title>;
+};
+
+Profiles.List = function ProfilesList({ children, ...restprops }) {
+  return <List {...restprops}>{children}</List>;
+};
+
+Profiles.User = function ProfilesUser({ children, ...restprops }) {
+  return <Item {...restprops}>{children}</Item>;
+};
+
+Profiles.Picture = function ProfilesPicture({ src, ...restprops }) {
+  return (
+    <Picture
+      {...restprops}
+      src={src ? `/images/users/${src}.png` : `/images/misc/loading.gif`}
+    />
+  );
+};
+
+Profiles.Name = function ProfilesName({ children, ...restprops }) {
+  return <Name {...restprops}>{children}</Name>;
+};
